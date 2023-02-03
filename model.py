a747e52d33cddbb829c3762832e9cb86728edf98
HyunSBong <sanseng@mju.ac.kr>
2023-02-03 18:45:41 +0900

    modify model and fixed tensor

--- model.py
+++ model.py
@@ -40,18 +40,33 @@ class VAE(nn.Module):
             decoder_layer_sizes, latent_size, conditional, num_labels)
 
     def forward(self, x, c=None):
+        # print('VAE forward')
         view_size = 1000
         if x.dim() > 2:
             x = x.view(-1, view_size)
 
         batch_size = x.size(0)
+        
+        if x.is_cuda != True:
+            x = x.cuda()
+        if c.is_cuda != True:
+            c = c.cuda()
+        # print(f'in VAE forward, x cuda? : {x.is_cuda} {x.dtype}')
 
         means, log_var = self.encoder(x, c)
+        # print(f'in VAE forward, means cuda? : {means.is_cuda} {means.dtype}')
+        # print(f'in VAE forward, log_var cuda? : {log_var.is_cuda} {log_var.dtype}')
 
-        std = torch.exp(0.5 * log_var).cuda()
-        eps = torch.randn([batch_size, self.latent_size]).cuda()
+        std = torch.exp(0.5 * log_var)
+        eps = torch.randn([batch_size, self.latent_size])
+        if eps.is_cuda != True:
+            eps = eps.cuda()
+        # print(f'in VAE forward, std cuda? : {std.is_cuda} {std.dtype}')
+        # print(f'in VAE forward, eps cuda? : {eps.is_cuda} {eps.dtype}')
         z = eps * std + means
-
+        
+        # print(f'in VAE forward, z cuda? : {z.is_cuda} {z.dtype}')
+        # print(f'in VAE forward, c cuda? : {c.is_cuda} {c.dtype}')
         recon_x = self.decoder(z, c)
 
         return recon_x, means, log_var, z
@@ -68,12 +83,15 @@ class VAE(nn.Module):
 
     def embedding(self, x, c=None):
         view_size = 1000
+        #if x.dim() > 2:
+        #    x = x.view(-1, view_size)
 
         batch_size = x.size(0)
 
         means, log_var = self.encoder(x, c)
         std = torch.exp(0.5 * log_var)
         eps = torch.randn([1, self.latent_size])
+        
         z = eps * std + means
 
         return z
@@ -82,6 +100,7 @@ class VAE(nn.Module):
 class Encoder(nn.Module):
 
     def __init__(self, layer_sizes, latent_size, conditional, num_labels):
+        # print('E init')
 
         super().__init__()
 
@@ -102,17 +121,18 @@ class Encoder(nn.Module):
         self.linear_log_var = nn.Linear(layer_sizes[-1], latent_size)
 
     def forward(self, x, c=None):
+        # print('E forward')
+        # print(f'in E forward, x cuda? : {x.is_cuda} {x.dtype}')
+        # print(f'in E forward, c cuda? : {c.is_cuda} {c.dtype}')
         if self.conditional:
-            print(c.is_cuda)
-            print(x.is_cuda)
             c = idx2onehot(c, n=self.num_labels)
             x = torch.cat((x, c), dim=-1)
-        # print(f'여기? x.shape : {x.shape}')
-        # print(self.MLP)
-        # x = torch.rand(128,1015).cuda()
+        # print(f'in E forward, x cuda? : {x.is_cuda}')
         x = self.MLP(x)
         means = self.linear_means(x)
         log_vars = self.linear_log_var(x)
+        # print(f'in E forward, means cuda? : {means.is_cuda} {means.dtype}')
+        # print(f'in E forward, log_vars cuda? : {log_vars.is_cuda} {log_vars.dtype}')
 
         return means, log_vars
 
@@ -120,6 +140,7 @@ class Encoder(nn.Module):
 class Decoder(nn.Module):
 
     def __init__(self, layer_sizes, latent_size, conditional, num_labels):
+        # print('D init')
 
         super().__init__()
 
@@ -141,9 +162,17 @@ class Decoder(nn.Module):
                 self.MLP.add_module(name="sigmoid", module=nn.Sigmoid())
 
     def forward(self, z, c):
+        # print('D forward')
         if self.conditional:
-            c = idx2onehot(c, n=self.num_labels)
+            # print(f'in D forward, c dtype? : {c.dtype} {type(c)}')
+            # print(f'in D forward, z dtype? : {z.dtype} {type(z)}')
+            if type(c) != torch.Tensor:
+                c = torch.from_numpy(c)
+            c = c.cuda()
             z = z.cuda()
+            # print(f'in D forward, c cuda? : {c.is_cuda} {c.dtype}')
+            # print(f'in D forward, z cuda? : {z.is_cuda} {z.dtype}')
+            c = idx2onehot(c, n=self.num_labels)
             z = torch.cat((z, c), dim=-1)
 
         x = self.MLP(z)
