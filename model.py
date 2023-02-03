d691cc35f72d3b7f4682e38c4ad4a73ebc77fbce
HyunSBong <sanseng@mju.ac.kr>
2023-02-04 00:39:06 +0900

    remove decs

--- model.py
+++ model.py
@@ -40,7 +40,6 @@ class VAE(nn.Module):
             decoder_layer_sizes, latent_size, conditional, num_labels)
 
     def forward(self, x, c=None):
-        # print('VAE forward')
         view_size = 1000
         if x.dim() > 2:
             x = x.view(-1, view_size)
@@ -51,22 +50,14 @@ class VAE(nn.Module):
             x = x.cuda()
         if c.is_cuda != True:
             c = c.cuda()
-        # print(f'in VAE forward, x cuda? : {x.is_cuda} {x.dtype}')
 
         means, log_var = self.encoder(x, c)
-        # print(f'in VAE forward, means cuda? : {means.is_cuda} {means.dtype}')
-        # print(f'in VAE forward, log_var cuda? : {log_var.is_cuda} {log_var.dtype}')
 
         std = torch.exp(0.5 * log_var)
         eps = torch.randn([batch_size, self.latent_size])
         if eps.is_cuda != True:
             eps = eps.cuda()
-        # print(f'in VAE forward, std cuda? : {std.is_cuda} {std.dtype}')
-        # print(f'in VAE forward, eps cuda? : {eps.is_cuda} {eps.dtype}')
         z = eps * std + means
-        
-        # print(f'in VAE forward, z cuda? : {z.is_cuda} {z.dtype}')
-        # print(f'in VAE forward, c cuda? : {c.is_cuda} {c.dtype}')
         recon_x = self.decoder(z, c)
 
         return recon_x, means, log_var, z
@@ -96,7 +87,6 @@ class VAE(nn.Module):
 
         return z
 
-
 class Encoder(nn.Module):
 
     def __init__(self, layer_sizes, latent_size, conditional, num_labels):
@@ -112,7 +102,6 @@ class Encoder(nn.Module):
         self.num_labels = num_labels
 
         for i, (in_size, out_size) in enumerate(zip(layer_sizes[:-1], layer_sizes[1:])):
-            # print(f'layer_sizes의 (in_size, out_size) : {in_size, out_size}')
             self.MLP.add_module(
                 name="L{:d}".format(i), module=nn.Linear(in_size, out_size))
             self.MLP.add_module(name="A{:d}".format(i), module=nn.ReLU())
@@ -121,26 +110,18 @@ class Encoder(nn.Module):
         self.linear_log_var = nn.Linear(layer_sizes[-1], latent_size)
 
     def forward(self, x, c=None):
-        # print('E forward')
-        # print(f'in E forward, x cuda? : {x.is_cuda} {x.dtype}')
-        # print(f'in E forward, c cuda? : {c.is_cuda} {c.dtype}')
         if self.conditional:
             c = idx2onehot(c, n=self.num_labels)
             x = torch.cat((x, c), dim=-1)
-        # print(f'in E forward, x cuda? : {x.is_cuda}')
         x = self.MLP(x)
         means = self.linear_means(x)
         log_vars = self.linear_log_var(x)
-        # print(f'in E forward, means cuda? : {means.is_cuda} {means.dtype}')
-        # print(f'in E forward, log_vars cuda? : {log_vars.is_cuda} {log_vars.dtype}')
 
         return means, log_vars
-
 
 class Decoder(nn.Module):
 
     def __init__(self, layer_sizes, latent_size, conditional, num_labels):
-        # print('D init')
 
         super().__init__()
 
@@ -162,16 +143,11 @@ class Decoder(nn.Module):
                 self.MLP.add_module(name="sigmoid", module=nn.Sigmoid())
 
     def forward(self, z, c):
-        # print('D forward')
         if self.conditional:
-            # print(f'in D forward, c dtype? : {c.dtype} {type(c)}')
-            # print(f'in D forward, z dtype? : {z.dtype} {type(z)}')
             if type(c) != torch.Tensor:
                 c = torch.from_numpy(c)
             c = c.cuda()
             z = z.cuda()
-            # print(f'in D forward, c cuda? : {c.is_cuda} {c.dtype}')
-            # print(f'in D forward, z cuda? : {z.is_cuda} {z.dtype}')
             c = idx2onehot(c, n=self.num_labels)
             z = torch.cat((z, c), dim=-1)
 
