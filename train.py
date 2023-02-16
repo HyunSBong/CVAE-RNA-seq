b1da38708b7c78fa24c3b87b5d3d9d3003b6be38
HyunSBong <sanseng@mju.ac.kr>
2023-02-16 15:21:40 +0900

    change devops, add wandb

--- train.py
+++ train.py
@@ -39,7 +39,7 @@ def draw_umap(X_test, x_syn, Y_test_tissues, Y_test_datasets, normalize=False):
     emb_2d = model.transform(x)
     plot_umap(emb_2d, X_test, x_syn, Y_test_tissues, Y_test_datasets)
 
-def load_data(num_genes):
+def load_data(num_genes,multivariate):
     train = pd.read_csv(f'../data/RNAseqDB/x_train_all_tissue.csv')
     test = pd.read_csv(f'../data/RNAseqDB/x_test_all_tissue.csv')
     df_real = pd.concat([train, test])
@@ -61,9 +61,10 @@ def load_data(num_genes):
     std_scaler = StandardScaler().fit(X_train)
     X_train = std_scaler.transform(X_train)
     X_test = std_scaler.transform(X_test)
-    minmax_scaler = MinMaxScaler().fit(X_train)
-    X_train = minmax_scaler.transform(X_train)
-    X_test = minmax_scaler.transform(X_test)
+    if multivariate == 1:
+        minmax_scaler = MinMaxScaler().fit(X_train)
+        X_train = minmax_scaler.transform(X_train)
+        X_test = minmax_scaler.transform(X_test)
 
     dict_train_test_splited = {'train_set': (X_train, Y_train),
                             'test_set': (X_test, Y_test),
@@ -108,6 +109,10 @@ def generate_synthetic_n_save(vae_model, le, X, gene_names, Y_test_tissues, epoc
     return x_syn
 
 def main(args, rna_dataset):
+    # wandb
+    wandb.init(project="VAE", reinit=True)
+    wandb.config.update(args)
+    
     torch.manual_seed(args.seed)
     latest_loss = torch.tensor(1)
     
@@ -138,7 +143,7 @@ def main(args, rna_dataset):
     
     num_tissue = len(set(Y_tissues)) # 15 (breast, lung, liver 등 15개 tissue)
     view_size = X.shape[1]
-    log2pi = torch.log2(torch.Tensor([np.pi]))
+    log2pi = torch.log2(torch.Tensor([np.pi])).cuda()
     
     # one-hot encoding
     le = LabelEncoder()
@@ -159,27 +164,28 @@ def main(args, rna_dataset):
     
     # loss function
     mse_criterion = nn.MSELoss(size_average=False)
-    def loss_fn(mean, log_var, recon_x=None, z=None, z_mean=None, z_log_var=None, multivariate=False):
-        if multivariate:
-            # reconstruction error
-            reconstr_loss = mse_criterion(
-                z_mean.view(-1, view_size), x.view(-1, view_size), reduction='sum')
-            # reconstr_loss = log2pi + z_log_var + (x - z_mean) ** 2 / (2 * torch.exp(z_log_var))
+    def loss_fn_gaussian(x, mean, log_var, z_mean, z_log_var):
+        # reconstruction error
+        reconstr_loss = mse_criterion(
+            z_mean.view(-1, view_size), x.view(-1, view_size))
+        # ln(0)=-Infinity을 피하기 위해 1e-10 추가
+        # reconstr_loss = torch.sum((0.5 * (z_log_var)) + (x - z_mean) ** 2 / (2 * torch.exp(z_log_var)))
             
-            # Kullback-Leibler divergence
-            latent_loss = -0.5 * torch.sum(1 + log_var - mean.pow(2) - log_var.exp())
-            MSE = torch.sum(x - 1)**2 # ignore
+        # Kullback-Leibler divergence
+        kl_loss = -0.5 * torch.sum(1 + log_var - mean.pow(2) - log_var.exp())
+        elbo = (reconstr_loss + kl_loss) / x.size(0)
+        # x.size => (batch_size,18000)
+        
+        return {'elbo': elbo, 'reconstr_loss': reconstr_loss, 'kl_loss': kl_loss}
+        
+    def loss_fn_bernoulli(recon_x, x, mean, log_var):
+        # reconstruction error
+        reconstr_loss = torch.nn.functional.binary_cross_entropy(
+            recon_x.view(-1, view_size), x.view(-1, view_size), reduction='sum')
+        # Kullback-Leibler divergence
+        kl_loss = -0.5 * torch.sum(1 + log_var - mean.pow(2) - log_var.exp()) # e^log_var
             
-            return {'reconstr_latent_loss': (reconstr_loss + latent_loss) / x.size(0), 'MSE': MSE, 'reconstr_loss': reconstr_loss, 'latent_loss': latent_loss}
-        else:
-            # reconstruction error
-            reconstr_loss = torch.nn.functional.binary_cross_entropy(
-                recon_x.view(-1, view_size), x.view(-1, view_size), reduction='sum')
-            # Kullback-Leibler divergence
-            latent_loss = -0.5 * torch.sum(1 + log_var - mean.pow(2) - log_var.exp())
-            MSE = torch.sum((x - recon_x)**2)
-            
-            return {'reconstr_latent_loss': (reconstr_loss + latent_loss) / x.size(0), 'MSE': MSE, 'reconstr_loss': reconstr_loss, 'latent_loss': latent_loss}
+        return {'elbo': (reconstr_loss + kl_loss) / x.size(0),'reconstr_loss': reconstr_loss, 'kl_loss': kl_loss}
 
     vae = CVAE(
         data_dim=X_train.shape[1],
@@ -191,10 +197,8 @@ def main(args, rna_dataset):
         multivariate = multivariate,
         num_labels=num_tissue if args.conditional else 0).to(device)
 
-    dataiter = iter(train_loader)
-    genes, labels = dataiter.next()
-
     optimizer = torch.optim.Adam(vae.parameters(), lr=args.learning_rate)
+    wandb.watch(vae)
     
     # Train
     stop_point = 10
@@ -202,6 +206,8 @@ def main(args, rna_dataset):
     initial_stop_point = stop_point
     stop_point_done = False
     losses = []
+    
+    score = 0
 
     for epoch in range(args.epochs):
         train_epoch = epoch
@@ -209,125 +215,109 @@ def main(args, rna_dataset):
             train_epoch = epoch - 1
             break
         train_loss = 0
+        sum_elbo = 0
+        sum_kl_loss = 0
+        sum_reconstr_loss = 0
 
         for batch_idx, (x, y) in enumerate(train_loader):
-
             x, y = x.to(device), y.to(device)
-
+            if x.is_cuda != True:
+                x = x.cuda()
             if args.conditional and multivariate:
-                if x.is_cuda != True:
-                    x = x.cuda()
                 mean, log_var, z_mean, z_log_var = vae(x, y)
-                losses = loss_fn(
-                    mean = mean, 
-                    log_var = log_var, 
-                    z_mean = z_mean, 
-                    z_log_var = z_log_var, 
-                    multivariate = multivariate)
+                losses = loss_fn_gaussian(x, mean, log_var, z_mean, z_log_var)
+                    
             elif args.conditional and multivariate==False:
-                if x.is_cuda != True:
-                    x = x.cuda()
                 recon_x, mean, log_var, z = vae(x, y)
-                losses = loss_fn(
-                    mean = mean, 
-                    log_var = log_var, 
-                    recon_x = recon_x, 
-                    z = z, 
-                    multivariate = multivariate)
+                losses = loss_fn_bernoulli(recon_x, x, mean, log_var)
             else:
                 recon_x, mean, log_var, z = vae(x)
-                losses = loss_fn(
-                    mean = mean, 
-                    log_var = log_var, 
-                    recon_x = recon_x, 
-                    z = z, 
-                    multivariate = multivariate)
-                    
-            loss = losses['reconstr_latent_loss'].clone() #  KL-Divergence + reconstruction error / x.size(0)
+                losses = loss_fn_bernoulli(recon_x, x, mean, log_var)
+            
+            loss = losses['elbo'].clone() #  KL-Divergence + reconstruction error / x.size(0)
             train_loss += loss
-
-            # latest_loss = losses['MSE'].detach()
-
+            
+            sum_elbo += losses['elbo']
+            sum_kl_loss += losses['kl_loss']
+            sum_reconstr_loss += losses['reconstr_loss']
+            
             optimizer.zero_grad()
-            
-            # writer.add_scalar("Train/Reconstruction Error",losses['BCE'].detach().item(), batch_idx + epoch * (len(train_loader.dataset)/args.batch_size) )
-            # writer.add_scalar("Train/KL-Divergence", losses['KLD'].detach().item(), batch_idx + epoch * (len(train_loader.dataset)/args.batch_size) )
-            # writer.add_scalar("Train/Total Loss" , loss.item(), batch_idx + epoch * (len(train_loader.dataset)/args.batch_size) )
-            
             loss.backward(retain_graph=True)
             optimizer.step()
 
-            # logs['loss'].append(loss.item())
+        c = torch.from_numpy(test_labels) # le.fit_transform(Y_train_tissues)
+        x = vae.inference(n=c.size(0), c=c)
+        score = score_fn(X_test, x.detach().cpu().numpy())
+        # wandb.log({
+        #     "ELBO": loss.item(),
+        #     "Reconstruction Error": losses['reconstr_loss'].item(),
+        #     "KL-Divergence": losses['kl_loss'].item(),
+        #     "Gamma Score": score
+        # })
+        if epoch % 5 == 0:
+            print(f'stop point : {stop_point}')
+            if score > best_score:
+                best_score = score
+                stop_point = initial_stop_point
+                # x_syn = save_synthetic(vae, x, train_epoch, 'trial_010', X.shape[1])
+                # x_syn = generate_synthetic_n_save(vae, le, X, gene_names, Y_test_tissues, train_epoch, 'trial_004', X.shape[1])
+            else:
+                stop_point -= 1
             
-            if batch_idx % 100 == 0 or batch_idx == len(train_loader)-1:
-                print("Epoch {:02d}/{:02d} Batch {:04d}/{:d}, Loss {:9.4f}".format(
-                    epoch, args.epochs, batch_idx, len(train_loader)-1, loss.item()))
-
-                if args.conditional:
-                    if epoch % 5 == 0:
-                        # generate gene
-                        c = torch.from_numpy(test_labels) # le.fit_transform(Y_train_tissues)
-                        x = vae.inference(n=c.size(0), c=c)
-                        if multivariate:
-                            mean, log_var, z_mean, z_log_var = vae(x, y)
-                        elif multivariate == False:
-                            x = vae.inference(n=c.size(0), c=c)
-                        score = score_fn(X_test, x.detach().cpu().numpy())
-                        if score > best_score:
-                            best_score = score
-                            stop_point = initial_stop_point
-                            x_syn = save_synthetic(vae, x, train_epoch, 'trial_005', X.shape[1])
-                            # x_syn = generate_synthetic_n_save(vae, le, X, gene_names, Y_test_tissues, train_epoch, 'trial_004', X.shape[1])
-                        else:
-                            stop_point -= 1
-                        print(f'==>Gamma Score : {score}')
-                        print(f'==>stop_point : {stop_point}')
-                else:
-                    x = vae.inference()
-                    
-            if stop_point == 0:
-                train_epoch = epoch
-                x_syn = save_synthetic(vae, x, train_epoch, 'trial_005', X.shape[1])
-                # x_syn = generate_synthetic_n_save(vae, le, X, gene_names, Y_test_tissues, train_epoch, 'trial_005', X.shape[1])
-                stop_point_done = True
+        avg_loss = sum_elbo / len(train_loader)
+        avg_kl_loss = sum_kl_loss / len(train_loader)
+        avg_reconstr_loss = sum_reconstr_loss / len(train_loader)
         
+        print("Epoch {:02d}/{:02d} Loss {:9.4f}, KL {:9.4f}, Reconstruction {:9.4f}".format(
+            epoch, args.epochs, avg_loss, avg_kl_loss, avg_reconstr_loss))
+        print(f'==>Gamma Score : {score}')
+        wandb.log({
+            "ELBO": avg_loss,
+            "Reconstruction Error": avg_reconstr_loss,
+            "KL-Divergence": avg_kl_loss,
+            "Gamma Score": score
+        })
+        if stop_point == 0:
+            train_epoch = epoch
+            # x_syn = save_synthetic(vae, x, train_epoch, 'trial_010', X.shape[1])
+            # x_syn = generate_synthetic_n_save(vae, le, X, gene_names, Y_test_tissues, train_epoch, 'trial_005', X.shape[1])
+            stop_point_done = True
+
     with torch.no_grad():
         for epoch in range(args.epochs):
             test_loss = 0
             for batch_idx, (x, y) in enumerate(test_loader):
-                recon_x, mean, log_var, z = vae(x, y)
                 if x.is_cuda != True:
                     x = x.cuda()
-                    
-                losses = loss_fn(recon_x, x, mean, log_var)
-                # writer.add_scalar("Test/Reconstruction Error",losses['BCE'].detach().item(), batch_idx + epoch * (len(test_loader.dataset)/args.batch_size) )
-                # writer.add_scalar("Test/KL-Divergence", losses['KLD'].detach().item(), batch_idx + epoch * (len(test_loader.dataset)/args.batch_size) )
-                # writer.add_scalar("Test/Total Loss" , loss.item(), batch_idx + epoch * (len(test_loader.dataset)/args.batch_size) )
-                test_loss = losses['GL'].clone()
+                if multivariate:
+                    losses = loss_fn_gaussian(x, mean, log_var, z_mean, z_log_var)
+                elif multivariate == False:
+                    losses = loss_fn_bernoulli(recon_x, x, mean, log_var)
+                test_loss = losses['elbo'].clone()
 
                 if batch_idx == len(test_loader) - 1:
                     print('====> Test set loss: {:.4f}'.format(test_loss.item()))
 
-    # writer.close()
+    x_syn = generate_synthetic_n_save(vae, le, X, gene_names, Y_test_tissues, train_epoch, 'trial_001', X.shape[1])
     draw_umap(X_test, x_syn, Y_test_tissues, Y_test_datasets)
 
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
     parser.add_argument("--seed", type=int, default=0)
-    parser.add_argument("--epochs", type=int, default=200)
-    parser.add_argument("--batch_size", type=int, default=128)
-    parser.add_argument("--learning_rate", type=float, default=0.001)
+    parser.add_argument("--epochs", type=int, default=300)
+    parser.add_argument("--batch_size", type=int, default=194)
+    parser.add_argument("--learning_rate", type=float, default=0.0001) # bernoulli 0.001
     parser.add_argument("--l2scale",type=float, default=0.00001)
     parser.add_argument("--compress_dims", type=list, default=[1000, 512, 256])
     parser.add_argument("--decompress_dims", type=list, default=[256, 512, 1000])
     parser.add_argument("--latent_size", type=int, default=50)
-    parser.add_argument("--num_genes", type=int, default=1000)
     parser.add_argument("--conditional", action='store_true', default=True)
     parser.add_argument("--gpu_id", type=int, default=0)
-    parser.add_argument("--multivariate", type=int, default=0)
+    parser.add_argument("--num_genes", type=int, default=1000)
+    parser.add_argument("--multivariate", type=int, default=1)
 
     args = parser.parse_args()
     
-    rna_dataset = load_data(args.num_genes)
+    rna_dataset = load_data(args.num_genes, args.multivariate)
     main(args, rna_dataset)
